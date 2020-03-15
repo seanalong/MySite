6c9cc0af2b8d432d3073c2f5ea0166323398fa5f
seanalong <s.anthony.long@gmail.com>
2020-03-15 20:08:28 +0000

    added create/update functionallity. Added some css to the table to make it more readable

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,13 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * This the default generated controller for CodeIngiter.
+ * - I have changed it to be the controller Runs instead - please see that file instead
+ */
 class Welcome extends CI_Controller
 {
 
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('RunsModel');
 	}
 
 	/**
@@ -25,9 +28,7 @@ class Welcome extends CI_Controller
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$data['runs'] = $this->RunsModel->selectAll();
-		$this->load->view('Runs/RunsOutput', $data);
-	}
+	public function index(){}
+	
 }
+
--- application/views/Runs/RunsOutput.php
+++ application/views/Runs/RunsOutput.php
@@ -1,13 +1,69 @@
+<style>
+	.container-table-runs {
+		width: 50%;
+		height: 300px;
+		border-collapse: collapse;
+		overflow-x: auto;
+	}
+	
+	.table-runs {
+		width: 100%;
+	}
+	
+	.table-runs td, th {
+		border: 1px solid #ddd;
+		padding: 8px;
+	}
+	
+	.table-runs tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
+	
+	.table-runs tr:hover {
+		background-color: #ddd;
+	}
+	
+	.table-runs th {
+		padding-top: 12px;
+		padding-bottom: 12px;
+		text-align: left;
+		background-color: #2b2d2f;
+		color: white;
+	}
+	
+	.message {
+		width: 100%;
+		height: 30px;
+		line-height: 30px;
+	}
+	
+	.message-success {
+		border: solid 1px green;
+		background-color: #90ee90;
+		color: green;
+	}
+	
+	.message-error {
+		border: solid 1px red;
+		background-color: #ffcccb;
+		color: red;
+	}
+</style>
+<?php if (!empty($message)) { ?>
+	<div class="message <?php echo $messageClass; ?>">
+		<?php echo $message; ?>		
+	</div>
+<?php } ?>
 <?php if (!empty($runs)) { ?>
 	<h2>Currently Stored Train Runs</h2>
-	<div>
-		<table>
+	<div class="container-table-runs">
+		<table class="table-runs">
 			<thead>
 				<tr>
-					<th>Train Line</th>
-					<th>Train Route</th>
-					<th>Run Number</th>
-					<th>Operator</th>
+					<th id="trainLineHeader" data-index="trainLine">Train Line</th>
+					<th id="trainRouteHeader" data-index="route">Train Route</th>
+					<th id="trainIdHeader" data-index="id">Run Number</th>
+					<th id="trainOperatorHeader" data-index="operator">Operator</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -23,3 +79,25 @@
 		</table>
 	</div>
 <?php } ?>
+<h3>Create/Update A Run</h3>
+<div class="runs-form-container">
+	<form action="<?php echo $runPostEndpoint; ?>" method="post">
+		<div>
+			<label for="runId">Run Number</label>
+			<input type="text" id="runId" name="runId">
+		</div>
+		<div>
+			<label for="route">Train Route</label>
+			<input type="text" id="route"  name="route">
+		</div>
+		<div>
+			<label for="trainLine">Train Line</label>
+			<input type="text" id="trainLine" name="trainLine">
+		</div>
+		<div>
+			<label for="operator">Operator</label>
+			<input type="text" id="operator" name="operator">
+		</div>
+		<input id="run-form-submit" type="submit" value="Create New Run">
+	</form>
+</div>
